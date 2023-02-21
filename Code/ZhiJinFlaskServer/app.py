fcc2dd3bf01a33a86e3743117de775ddfacbfd53
Ricardo2001ZG <1072294155@qq.com>
2023-02-22 02:45:09 +0800

    [ZhiJinFlaskServer][Feature]Debugmode auto reload
    1.已支持 flask run 命令进行开发
    2.dev_environment 环境下开发已自动设置为调试模式

--- Code/ZhiJinFlaskServer/app.py
+++ Code/ZhiJinFlaskServer/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from lib_app.models import db
 from lib_app.routes import routes
 
-if __name__ == '__main__':
+def create_app():
     app = Flask(__name__)
     # Load configuration from external source here
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -20,7 +20,10 @@ if __name__ == '__main__':
 
     for rule in app.url_map.iter_rules():
         print(rule, rule.methods)
-    
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
     debug_mode = os.getenv("FLASK_DEBUG") == "True"
     port = int(os.getenv("FLASK_PORT", 5000))
     app.run(debug=debug_mode, port=port)
--- Code/ZhiJinFlaskServer/dev_environment.bat
+++ Code/ZhiJinFlaskServer/dev_environment.bat
@@ -1,3 +1,4 @@
 @echo off
+set FLASK_DEBUG=1
 cmd /K "venv_zhijin_flask_server\Scripts\activate.bat"
 @echo on
