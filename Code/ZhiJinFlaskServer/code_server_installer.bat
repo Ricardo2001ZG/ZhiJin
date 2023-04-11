dc063cc809cc94deaabd21ece9d01545726fcaef
Ricardo2001ZG <1072294155@qq.com>
2023-04-11 09:26:20 +0800

    [ZhiJinFlaskServer][Update]更新 .gitignore
    1. 更新 .gitignore

---
+++ Code/ZhiJinFlaskServer/code_server_installer.bat
@@ -0,0 +1,7 @@
+set PATH=%PATH%;%cd%\env\python-3.11.2-embed-amd64;%cd%\env\python-3.11.2-embed-amd64\Scripts;
+cmd /c "env\python-3.11.2-embed-amd64\python -m pip install -i https://mirrors.cloud.tencent.com/pypi/simple --upgrade pip"
+cmd /c "pip config set global.index-url https://mirrors.cloud.tencent.com/pypi/simple"
+cmd /c "pip install virtualenv"
+cmd /c "virtualenv venv_zhijin_flask_server"
+cmd /c "venv_code_server\Scripts\activate.bat"
+pause
